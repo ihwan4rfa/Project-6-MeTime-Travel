72759fd109bd254c738ef6ed6dfe191a77a0fbe3
ihwan4rfa <ihwanarifandi@gmail.com>
2024-07-01 05:56:50 +0700

    fix: Resolve multiple bugs including role input value to API, change button type to prevent unintended form submit, and active navbar link UI

--- src/components/Layout/LoginRegisterForm.js
+++ src/components/Layout/LoginRegisterForm.js
@@ -50,7 +50,7 @@ const LoginRegisterForm = ({ onSubmit }) => {
             name: e.target.name.value,
             password: e.target.password.value,
             passwordRepeat: e.target.password.value,
-            role: selectedOption2,
+            role: selectedOption2.toLowerCase(),
             profilePictureUrl: profilePictureUrl,
             phoneNumber: e.target.phoneNumber.value
         };
@@ -121,11 +121,11 @@ const LoginRegisterForm = ({ onSubmit }) => {
                                 <Dropdown selectedOption2={selectedOption2} setSelectedOption2={setSelectedOption2} />
                                 <div className="flex bg-slate-200 my-2 py-[10px] px-4 text-[13px] rounded-lg w-full">
                                     <input type={seePassword ? 'text' : 'password'} name="password" id="password" placeholder="Password" className="w-full outline-none bg-slate-200 placeholder:text-slate-400 text-primaryblack" />
-                                    <button onClick={toggleSeePassword}><i className={`text-slate-400 pl-3 fa-solid ${seePassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
+                                    <button type="button" onClick={toggleSeePassword}><i className={`text-slate-400 pl-3 fa-solid ${seePassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
                                 </div>
                                 <div className="flex bg-slate-200 my-2 py-[10px] px-4 text-[13px] rounded-lg w-full">
                                     <input type={seeRepeatPassword ? 'text' : 'password'} name="passwordRepeat" id="passwordRepeat" placeholder="Repeat Password" className="w-full outline-none bg-slate-200 placeholder:text-slate-400 text-primaryblack" />
-                                    <button onClick={toggleSeeRepeatPassword}><i className={`text-slate-400 pl-3 fa-solid ${seeRepeatPassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
+                                    <button type="button" onClick={toggleSeeRepeatPassword}><i className={`text-slate-400 pl-3 fa-solid ${seeRepeatPassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
                                 </div>
                             </div>
                             <div className="flex gap-4 text-[10px]">
@@ -136,7 +136,7 @@ const LoginRegisterForm = ({ onSubmit }) => {
                         </div>
                         {tokenRegister ? <h1 className="text-[12px] text-teal-500"><i className="mr-1 fa-solid fa-circle-check"></i>registration success!</h1> : ""}
                         {errorRegister ? <h1 className="text-[12px] text-red-500 text-center"><i className="mr-1 fa-solid fa-triangle-exclamation"></i>{errorRegister}</h1> : ""}
-                        <button type="submit" className="bg-primaryred hover:bg-redhover text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Register</button>
+                        <button type="submit" className="bg-primaryblue hover:bg-bluehover text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Register</button>
                     </form>
                 </div>
                 <div className={`absolute top-0 md:h-full h-2/3 transition-all duration-500 ease-in-out md:w-1/2 w-full ${loginClicked ? 'z-20 translate-x-[0%]' : 'z-10 md:translate-x-[100%] -translate-x-[100%]'}`}>
@@ -150,7 +150,7 @@ const LoginRegisterForm = ({ onSubmit }) => {
                         </div>
                         {token ? <h1 className="text-[12px] text-teal-500"><i className="mr-1 fa-solid fa-circle-check"></i>log In success!</h1> : ""}
                         {errorLogin ? <h1 className="text-[12px] text-red-500 text-center"><i className="mr-1 fa-solid fa-triangle-exclamation"></i>Log In failed! {errorLogin}</h1> : ""}
-                        <button onClick={handleLogin} className="bg-primaryblue hover:bg-bluehover text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Log In</button>
+                        <button onClick={handleLogin} className="bg-primaryred hover:bg-redhover text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Log In</button>
                     </div>
                 </div>
                 <div className={`absolute md:top-0 top-1/2 ${loginClicked ? 'md:w-1/2 md:left-1/2' : 'md:w-1/3 md:left-1/3'}  left-0 w-full md:h-full h-1/3 overflow-hidden transition-all duration-500 ease-in-out z-30 md:translate-y-[0%] translate-y-[50%] rounded-t-[50px] ${btnClicked ? 'md:-translate-x-[100%] md:rounded-r-[100px] md:rounded-tl-[0px] rounded-r-[0px]' : 'md:translate-x-[0%] md:rounded-l-[100px] md:rounded-tr-[0px] rounded-l-[0px]'}`}>
@@ -158,12 +158,12 @@ const LoginRegisterForm = ({ onSubmit }) => {
                         <div className={`absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[0%]`}>
                             <h1 className="text-xl font-semibold tracking-tight md:text-2xl">Welcome Back!</h1>
                             <p className="md:text-sm text-xs md:my-5 my-1 mx-[25px]">Log in with email and password to start your travel adventure!</p>
-                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-primaryblue">Log In</button>
+                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-primaryred">Log In</button>
                         </div>
                         <div className={`absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[100%]`}>
                             <h1 className="text-xl font-semibold tracking-tight md:text-2xl">New here?</h1>
                             <p className="md:text-sm text-xs md:my-5 my-1 md:mx-[80px] mx-[25px]">Register now and connect with fellow travelers to explore new horizons!</p>
-                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-primaryred">Register</button>
+                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-primaryblue">Register</button>
                         </div>
                     </div>
                 </div>
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -16,11 +16,11 @@ const Navbar = () => {
 
     return (
         <nav className='fixed flex items-center justify-between w-full h-16 mb-12 bg-white shadow-lg shadow-slate-200 px-36 text-primaryblack font-poppins'>
-            <Image src="/images/Logo.png" width={130} height={80} />
+            <button onClick={() => router.push(linkList.Home)}><Image src="/images/Logo.png" width={130} height={80} /></button>
             <div className="flex items-center gap-16 text-sm font-medium">
                 {keys.map((key, index) => (
                     <div key={index}>
-                        <button onClick={() => router.push(linkList[key])} className={`cursor-pointer hover:text-primaryred ${currentPath === linkList[key] ? 'text-primaryred' : ''} ${key === "Login" ? "px-4 py-2 border hover:text-white hover:bg-primaryred rounded-lg border-primaryred" : ""}`}>{key}</button>
+                        <button onClick={() => router.push(linkList[key])} className={`cursor-pointer hover:text-primaryred ${currentPath === linkList[key] && currentPath === "/login_register" ? 'text-white bg-primaryred' : (currentPath === linkList[key] ? 'text-primaryred' : '')} ${key === "Login" ? "px-4 py-2 border hover:text-white hover:bg-primaryred rounded-lg border-primaryred" : ""}`}>{key}</button>
                     </div>
                 ))}
             </div>
