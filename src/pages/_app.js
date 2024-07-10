949d20791ed443f3eb4a39701684774958524780
ihwan4rfa <ihwanarifandi@gmail.com>
2024-07-11 05:56:44 +0700

    fix: Resolve favicon not displaying issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,9 @@ export default function App({ Component, pageProps }) {
     <Provider store={store}>
       <Head>
         <title>MeTime Travel</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </Head>
       <Component {...pageProps} />
