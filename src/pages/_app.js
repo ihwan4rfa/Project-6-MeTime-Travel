e16d010f83ffefcae80804a3d7e3f35d6064e7e9
ihwan4rfa <ihwanarifandi@gmail.com>
2024-07-10 21:40:35 +0700

    fix: Resolve favicon not displaying issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }) {
     <Provider store={store}>
       <Head>
         <title>MeTime Travel</title>
-        <link rel="icon" href="/favicon.svg" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </Head>
       <Component {...pageProps} />
