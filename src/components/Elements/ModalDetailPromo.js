1718625fc40c6fead6832436db8f570076babf90
ihwan4rfa <ihwanarifandi@gmail.com>
2024-08-05 00:01:11 +0700

    feat: Make modal detail promo responsive

--- src/components/Elements/ModalDetailPromo.js
+++ src/components/Elements/ModalDetailPromo.js
@@ -30,40 +30,41 @@ const ModalDetailPromo = ({ showDetailPromo, setShowDetailPromo, selectedPromo, 
         <>
             <div className={`absolute z-30 w-full h-full opacity-40 bg-primaryblack dark:bg-primarygray dark:opacity-60 ${showDetailPromo === true ? '' : 'hidden'}`}></div>
             <div className={`${showDetailPromo === true ? '' : 'hidden'} absolute z-30 flex items-center justify-center w-full h-full`}>
-                <div className={`bg-white dark:bg-primaryblack shadow-lg rounded-lg text-[13px] flex justify-center relative text-primaryblack dark:text-slate-200 h-fit w-fit`}>
+                <div className={`bg-white dark:bg-primaryblack shadow-lg rounded-lg text-[10px] lg:text-[11px] xl:text-[13px] flex justify-center relative text-primaryblack dark:text-slate-200 h-fit w-fit`}>
                     <div className='absolute flex justify-end w-full p-2'>
-                        <button onClick={handleCloseEditPromo} className='w-8 h-8 text-xl rounded-lg hover:text-primaryred dark:hover:text-primaryred'><i class=" fa-solid fa-xmark"></i></button>
+                        <button onClick={handleCloseEditPromo} className='w-8 h-8 text-base lg:text-lg xl:text-xl rounded-lg hover:text-primaryred dark:hover:text-primaryred'><i class=" fa-solid fa-xmark"></i></button>
                     </div>
-                    <div className={`flex flex-col items-center justify-center w-full h-full p-5`}>
-                        <div className='flex flex-col items-start justify-center w-full gap-4 h-fit'>
-                            <div className='flex w-full gap-4'>
+                    <div className={`flex flex-col items-center justify-center w-full h-full pt-4 sm:pt-5 p-5`}>
+                        <div className='flex flex-col items-start justify-center w-full h-fit'>
+                            <div className='flex flex-col items-center sm:items-start sm:flex-row w-full gap-4'>
+                                <h1 className='text-lg lg:text-xl xl:text-2xl font-semibold capitalize flex sm:hidden -mb-2'>{selectedPromo.title}</h1>
                                 {selectedPromo.imageUrl && (
-                                    <div className='w-[300px] overflow-hidden rounded-lg h-[200px]'>
+                                    <div className='w-[230px] xs:w-[300px] sm:w-[260px] xl:w-[300px] overflow-hidden rounded-lg h-[160px] lg:h-[180px] xl:h-[200px]'>
                                         {selectedPromo.imageUrl.startsWith("https://") && (selectedPromo.imageUrl.includes(".jpg") || selectedPromo.imageUrl.includes(".png") || selectedPromo.imageUrl.includes("images")) ?
                                             <img src={selectedPromo.imageUrl} className='object-cover w-full h-full'></img>
                                             : <Image src="/images/no-image.png" className='object-cover w-full h-full' width={500} height={500} alt='Unknown Profile' />
                                         }
                                     </div>
                                 )}
-                                <div className='flex flex-col w-[450px] gap-1 text-primaryblack dark:text-slate-200'>
-                                    <h1 className='text-2xl font-semibold capitalize'>{selectedPromo.title}</h1>
-                                    <div className='flex justify-between w-full py-2 pl-3 pr-2 mb-2 rounded-lg bg-slate-100 dark:bg-slate-700'>
+                                <div className='flex flex-col w-[230px] xs:w-[300px] lg:w-[380px] xl:w-[450px] gap-1 text-primaryblack dark:text-slate-200'>
+                                    <h1 className='text-lg lg:text-xl xl:text-2xl font-semibold capitalize hidden sm:flex'>{selectedPromo.title}</h1>
+                                    <div className='flex flex-col xs:flex-row justify-between w-full pt-2 pb-3 xs:pb-2 pl-3 pr-2 mb-2 rounded-lg bg-slate-100 dark:bg-slate-700 gap-2 xs:gap-0'>
                                         <div className='flex flex-col items-start justify-between'>
-                                            <h1 className='text-2xl font-semibold text-primaryblue'>{formatNumber(selectedPromo.promo_discount_price)}</h1>
-                                            <h1 className='text-[11px]'>
+                                            <h1 className='text-lg lg:text-xl xl:text-2xl font-semibold text-primaryblue'>{formatNumber(selectedPromo.promo_discount_price)}</h1>
+                                            <h1 className='text-[8px] lg:text-[9px] xl:text-[11px]'>
                                                 Spend at least <span className='font-semibold rounded-md text-primaryred dark:text-primaryyellow'>
                                                     {formatNumber(selectedPromo.minimum_claim_price)}
                                                 </span> to get this promo
                                             </h1>
                                         </div>
-                                        <div className='flex flex-col items-end justify-between'>
-                                            <h1 className='text-[11px]'>Promo Code</h1>
+                                        <div className='flex flex-col items-start xs:items-end justify-between gap-1 xs:gap-0'>
+                                            <h1 className='text-[8px] lg:text-[9px] xl:text-[11px]'>Promo Code</h1>
                                             <div className='px-3 py-1 rounded-md cursor-pointer h-fit bg-primaryblue hover:bg-bluehover'>
                                                 <button onClick={() => handleCopy(selectedPromo.promo_code)} className={`text-white ${promoCopied ? '' : 'uppercase'}`}><i class={`fa-solid mr-2 ${promoCopied ? 'fa-check' : 'fa-clone'}`}></i>{promoCopied ? 'Copied' : selectedPromo.promo_code}</button>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='flex flex-col w-full gap-2 overflow-y-scroll no-scrollbar max-h-[85px] h-fit'>
+                                    <div className='flex flex-col w-full gap-2 overflow-y-scroll no-scrollbar max-h-[100px] xs:max-h-[85px] h-fit'>
                                         <div className='flex w-auto gap-8 ml-3'>
                                             <h1 className='w-[35%] text-primarygray dark:text-slate-400'>Description</h1>
                                             <h1 className='w-[65%]'>{selectedPromo.description}</h1>
